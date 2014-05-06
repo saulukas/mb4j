d19ee09af169bfbe4d7d2824fe973f12f3cc5849
saulukas <saulukas@gmail.com>
2014-05-06 16:41:59 +0300

    constructing support for resources

--- mb4j-sample/mb4j-sample-liferay/src/main/java/org/mb4j/sample/liferay/zbox/TestbenchPortlet.java
+++ mb4j-sample/mb4j-sample-liferay/src/main/java/org/mb4j/sample/liferay/zbox/TestbenchPortlet.java
@@ -1,8 +1,11 @@
 package org.mb4j.sample.liferay.zbox;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
+import static javax.portlet.PortletSession.APPLICATION_SCOPE;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
@@ -11,11 +14,16 @@ import javax.portlet.ResourceResponse;
 import org.mb4j.sample.liferay.util.PortletDebugUtils;
 
 public class TestbenchPortlet extends GenericPortlet {
+  AtomicLong renderCount = new AtomicLong();
+
   @Override
   protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
     System.out.println("mmmmm: " + PortletDebugUtils.printParametersOf("render", this, request));
     response.getWriter().println(""
         + "\nTetbench...");
+    //
+    //   nice URLs
+    //
     PortletURL url1 = response.createRenderURL();
     url1.setParameter("mvcPath", "urlPath_2/your/view");
     response.getWriter().println(""
@@ -37,6 +45,23 @@ public class TestbenchPortlet extends GenericPortlet {
         + "<p>"
         + "    <a href=\"" + url3 + "\">Long URL</a>"
         + "</p>");
+    //
+    //   session attributes
+    //
+    boolean hadSession = request.getPortletSession(false) != null;
+    Object oldValue = request.getPortletSession().getAttribute("LIFERAY_SHARED_A123", APPLICATION_SCOPE);
+    String newValue = "mb" + renderCount.incrementAndGet();
+    request.getPortletSession().setAttribute("LIFERAY_SHARED_A123", newValue, APPLICATION_SCOPE);
+    response.getWriter().println(""
+        + "<p>Session existed: " + hadSession + " oldValue=" + oldValue + " newValue=" + newValue + "</p>");
+    response.getWriter().println(""
+        + "<pre>Application attributes: "
+        + separateWith("\n", request.getPortletSession().getAttributeMap(APPLICATION_SCOPE))
+        + "</pre>"
+        + "<pre>Portlet attributes: "
+        + separateWith("\n", request.getPortletSession().getAttributeMap())
+        + "</pre>"
+    );
   }
 
   @Override
@@ -45,4 +70,11 @@ public class TestbenchPortlet extends GenericPortlet {
     super.serveResource(request, response);
   }
 
+  private String separateWith(String separator, Map<String, Object> attributeMap) {
+    String result = "";
+    for (Map.Entry<String, Object> entry : attributeMap.entrySet()) {
+      result += separator + entry.getKey() + "=[" + entry.getValue() + "]";
+    }
+    return result;
+  }
 }
