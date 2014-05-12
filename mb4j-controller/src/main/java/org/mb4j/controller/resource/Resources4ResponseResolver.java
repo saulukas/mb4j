b379dcaf1e190cb802bb4056be89d533a12fc4b7
Uawe <saulukas@gmail.com>
2014-05-12 20:43:25 +0300

    Implementing Resources of Comonent similar to FormActions of Form

--- mb4j-controller/src/main/java/org/mb4j/controller/resource/Resources4ResponseResolver.java
+++ mb4j-controller/src/main/java/org/mb4j/controller/resource/Resources4ResponseResolver.java
@@ -1,9 +1,17 @@
 package org.mb4j.controller.resource;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import org.mb4j.controller.Component;
 
 public abstract class Resources4ResponseResolver {
   public Resources4Response resolveResourcesFor(Component component) {
-    return new Resources4Response();
+    Collection<Resource4Response> resources = new ArrayList<>();
+    for (Resource resource : component.getResources()) {
+      resources.add(new Resource4Response(resolveResourceUrl(component, resource), resource));
+    }
+    return new Resources4Response(resources);
   }
+
+  protected abstract String resolveResourceUrl(Component component, Resource resource);
 }
---
+++ mb4j-servlet/src/main/java/org/mb4j/servlet/ServletResources4ResponseResolver.java
@@ -0,0 +1,4 @@
+package org.mb4j.servlet;
+
+public class ServletResources4ResponseResolver {
+}
