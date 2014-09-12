058771234dd5306fa7867d7ec4184aa78bd23e57
saulukas <saulukas@gmail.com>
2014-09-12 17:56:37 +0300

    Two more events added to sample database for more pleasant testing.

--- mb4j-example/mb4j-example-domain/src/main/java/org/mb4j/example/domain/EventDatabase.java
+++ mb4j-example/mb4j-example-domain/src/main/java/org/mb4j/example/domain/EventDatabase.java
@@ -10,5 +10,9 @@ public class EventDatabase {
     public final static List<Event> EVENT_LIST = new ArrayList<>(asList(
             new Event(1, "img/glyphicons_254_fishes.png", "Fishing", "Let's go fishing on saturday."),
             new Event(2, "img/glyphicons_314_table_tennis.png", "Ping pong", "Ping pong is on fridays."),
-            new Event(3, "img/glyphicons_338_turtle.png", "Slow mondays", "Mondays are for slow motion.")));
+            new Event(3, "img/glyphicons_338_turtle.png", "Slow mondays", "Mondays are for slow motion."),
+            new Event(4, "img/glyphicons_262_spade.png", "Healthy sundays", "Gardening with beer."),
+            new Event(5, "img/glyphicons_326_piano.png", "Fly away", "Jamming with friends.")
+    ));
+
 }
